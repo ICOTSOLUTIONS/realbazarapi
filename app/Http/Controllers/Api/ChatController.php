1c6a282fbb8239a473feb51510b08b2c9cc4d9b9
umermuk <umerwell1234@gmail.com>
2022-10-17 17:03:55 +0500

    working'

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Chat;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ChatController extends Controller
 {
@@ -19,7 +20,8 @@ class ChatController extends Controller
         })->orWhere(function ($q) use ($user_id) {
             $q->where('receiver_id', $user_id);
         })->get();
-        return response()->json(['Message' => "Done", 'chat' => $chat]);
+        if (count($chat)) return response()->json(['status' => true, 'Message' => "Chat Found", 'chat' => $chat], 200);
+        else return response()->json(['status' => false, 'Message' => "Chat not Found", 'chat' => $chat]);
     }
 
     public function chat(Request $request)
@@ -32,46 +34,54 @@ class ChatController extends Controller
         })->orWhere(function ($q) use ($receiver_id, $user_id) {
             $q->where('sender_id', $receiver_id)->where('receiver_id', $user_id);
         })->first();
-        if (!is_object($chat)) {
-            $receiver = User::find($receiver_id);
-            // $chat = new Chat();
-            // $chat->sender_id = $user_id;
-            // $chat->receiver_id = $receiver_id;
-            // if ($chat->save()) {
-            //     $chat = Chat::with(['sender', 'receiver', 'messages'])->where(function ($q) use ($user_id, $receiver_id) {
-            //         $q->where('sender_id', $user_id)->where('receiver_id', $receiver_id);
-            //     })->orWhere(function ($q) use ($receiver_id, $user_id) {
-            //         $q->where('sender_id', $receiver_id)->where('receiver_id', $user_id);
-            //     })->first();
-            // }
-            return response()->json(['Message' => "Done", 'chat' => $chat, 'sender' => $user, 'receiver' => $receiver]);
-        }
-        return response()->json(['Message' => "Done", 'chat' => $chat]);
+        if (!is_object($chat)) return response()->json(['status' => false, 'Message' => "Chat not Found", 'chat' => $chat]);
+        // {
+        // $receiver = User::find($receiver_id);
+        // $chat = new Chat();
+        // $chat->sender_id = $user_id;
+        // $chat->receiver_id = $receiver_id;
+        // if ($chat->save()) {
+        //     $chat = Chat::with(['sender', 'receiver', 'messages'])->where(function ($q) use ($user_id, $receiver_id) {
+        //         $q->where('sender_id', $user_id)->where('receiver_id', $receiver_id);
+        //     })->orWhere(function ($q) use ($receiver_id, $user_id) {
+        //         $q->where('sender_id', $receiver_id)->where('receiver_id', $user_id);
+        //     })->first();
+        // }
+
+        // }
+        return response()->json(['status' => true, 'Message' => "Done", 'chat' => $chat], 200);
     }
 
     public function message(Request $request)
     {
-        $user = auth()->user();
-        $user_id = $user->id;
-        $receiver_id = $request->receiver_id;
-        $chat = Chat::with(['sender', 'receiver', 'messages'])->where(function ($q) use ($user_id, $receiver_id) {
-            $q->where('sender_id', $user_id)->where('receiver_id', $receiver_id);
-        })->orWhere(function ($q) use ($receiver_id, $user_id) {
-            $q->where('sender_id', $receiver_id)->where('receiver_id', $user_id);
-        })->first();
-        if (!is_object($chat)) {
-            $chat = new Chat();
-            $chat->sender_id = $user_id;
-            $chat->receiver_id = $receiver_id;
-            if ($chat->save()) {
-                $chat = Chat::with(['sender', 'receiver', 'messages'])->where(function ($q) use ($user_id, $receiver_id) {
-                    $q->where('sender_id', $user_id)->where('receiver_id', $receiver_id);
-                })->orWhere(function ($q) use ($receiver_id, $user_id) {
-                    $q->where('sender_id', $receiver_id)->where('receiver_id', $user_id);
-                })->first();
+        try {
+            DB::beginTransaction();
+            $user = auth()->user();
+            $user_id = $user->id;
+            $receiver_id = $request->receiver_id;
+            $chat = Chat::with(['sender', 'receiver', 'messages'])->where(function ($q) use ($user_id, $receiver_id) {
+                $q->where('sender_id', $user_id)->where('receiver_id', $receiver_id);
+            })->orWhere(function ($q) use ($receiver_id, $user_id) {
+                $q->where('sender_id', $receiver_id)->where('receiver_id', $user_id);
+            })->first();
+            if (!is_object($chat)) {
+                $chat = new Chat();
+                $chat->sender_id = $user_id;
+                $chat->receiver_id = $receiver_id;
+                if ($chat->save()) {
+                    $chat = Chat::with(['sender', 'receiver', 'messages'])->where(function ($q) use ($user_id, $receiver_id) {
+                        $q->where('sender_id', $user_id)->where('receiver_id', $receiver_id);
+                    })->orWhere(function ($q) use ($receiver_id, $user_id) {
+                        $q->where('sender_id', $receiver_id)->where('receiver_id', $user_id);
+                    })->first();
+                }
             }
+            event(new MessageEvent($user->name, $request->message, $chat->id));
+            DB::commit();
+            return response()->json(['status' => true, 'Message' => "Chat Found", 'chat' => $chat], 200);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json(['status' => false, 'Message' => $th->getMessage()]);
         }
-        event(new MessageEvent($user->name, $request->message, $chat->id));
-        return response()->json(['Message' => "done", 'chat' => $chat]);
     }
 }
---
+++ app/Http/Controllers/Api/NotificationSend.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+  class Handle{
+
+static  function sendNotif($token,$title,$msg){
+
+            $from = "AAAAdMPQRUw:APA91bGqNp74BHXYOEMoTB2VcNmae-2X-sNgSIf8YqPFb5Q5UOzKfinCVeiqHxErT53BdjFN0yY_vHLHE34z_m11pNZYAt_p0kpmvaLwexkA0pj4FWxWaE9yXWysvyERF6MMjisjgala";
+            $msg = array
+              (
+                'body'  => "$msg",
+                'title' => "$title",
+                'receiver' => ' ',
+                'icon'  => "https://image.flaticon.com/icons/png/512/270/270014.png",/*Default Icon*/
+                'sound' => 'mySound'/*Default sound*/
+              );
+
+        $fields = array
+                (
+                    'to'        => $token,
+                    'notification'  => $msg
+                );
+
+        $headers = array
+                (
+                    'Authorization: key=' . $from,
+                    'Content-Type: application/json'
+                );
+        //#Send Reponse To FireBase Server
+        $ch = curl_init();
+        curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
+        curl_setopt( $ch,CURLOPT_POST, true );
+        curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+        curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+        curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+        $result = curl_exec($ch );
+        curl_close( $ch );
+
+
+    }
+}
+
+?>
