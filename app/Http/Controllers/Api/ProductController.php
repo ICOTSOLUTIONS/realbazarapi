83dad3848eea316a32db4f99158151fc75ee69bf
umermuk <umerwell1234@gmail.com>
2022-10-26 21:29:03 +0500

    Working

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -484,10 +484,10 @@ class ProductController extends Controller
 
     public function productStatusChange($id)
     {
-        $product = Product::has('user')->with(['user', 'images', 'subCategories.categories', 'reviews.users'])->where('id', $id)->where('is_delete', false)->first();
+        $product = Product::has('user')->with(['user', 'images', 'subCategories.categories', 'reviews.users'])->where('id', $id)->where('is_delete', false)->get();
         if ($product->status == true) $product->status = false;
         else $product->status = true;
-        if ($product->save()) return response()->json(["status" => true, 'Message' => 'Product Status Change Successfully', 'Products' => $product??[]], 200);
+        if ($product->save()) return response()->json(["status" => true, 'Message' => 'Product Status Change Successfully', 'Products' => ProductsResource::collection($product)??[]], 200);
         else return response()->json(["status" => false, 'Message' => 'Product Status Change not Successfully', 'Products' => $product ?? []]);
     }
 
