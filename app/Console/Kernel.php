054df6f100be54a916d08d86550070d56c541a50
umermuk <umerwell1234@gmail.com>
2022-10-30 02:58:12 +0500

    working

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        Commands\NewArrivalProductCommand::class,
+        Commands\SellerStatusCheckCommand::class,
+    ];
     /**
      * Define the application's command schedule.
      *
