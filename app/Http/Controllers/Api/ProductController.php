a6cc45620b990e77a64ec8e9d500e2854680a190
umermuk <umerwell1234@gmail.com>
2022-11-02 03:22:05 +0500

    working

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -326,9 +326,10 @@ class ProductController extends Controller
             if ($user->role->name == 'wholesaler' || $user->role->name == 'retailer') {
                 $payment = PackagePayment::where('user_id', $user->id)->where('end_date', '<', Carbon::now())->first();
                 // $payment exist means expired payment;
-                if ($payment == null || $user->is_active == false) throw new Error("Please buy package!");
+                if ($payment || $user->is_active == false) throw new Error("Please buy package!");
                 $productCount = Product::where('user_id', $user->id)->count();
                 $packageProductCount = PackagePayment::where('user_id', $user->id)->first();
+                if (!$packageProductCount) throw new Error("Please buy package!");
                 $qty = $packageProductCount->updated_product_qty;
                 if ($productCount >= $qty) throw new Error("Your Product limit is full now you buy new package!");
                 $new_product = new Product();
