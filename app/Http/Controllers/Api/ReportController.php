96007c2160ec2d5c394656151fe83b960e66afed
umermuk <umerwell1234@gmail.com>
2022-11-03 23:20:45 +0500

    working

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -12,7 +12,7 @@ class ReportController extends Controller
 {
     public function report()
     {
-        $report_count = Report::selectRaw('user_id, count(user_id) AS total')->groupBy('user_id')->get();
+        $report_count = Report::with('users:id,username')->selectRaw('user_id, count(user_id) AS total')->groupBy('user_id')->get();
         $reports = Report::with(['users', 'shop'])->withCount('users')->get();
         if (count($reports)) return response()->json(['status' => true, 'Message' => 'Reports found', 'reports' => $reports ?? [], 'count' => $report_count ?? []], 200);
         else return response()->json(['status' => false, 'Message' => 'Reports not found', 'reports' => $reports ?? []]);
