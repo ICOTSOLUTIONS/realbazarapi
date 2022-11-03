d4a0dc6784b4fce40400006a154537d75c7b7ca5
umermuk <umerwell1234@gmail.com>
2022-11-03 23:42:00 +0500

    working

--- app/Http/Resources/ProductsResource.php
+++ app/Http/Resources/ProductsResource.php
@@ -30,7 +30,7 @@ class ProductsResource extends JsonResource
             'is_new_arrival' => $this->is_new_arrival,
             'variation' => $this->variation,
             'image' => $this->images,
-            'shop' => $this->user,
+            'shop' => $this->user->role,
             'category' => $this->subCategories->categories,
             'sub_category' => $this->subCategories,
             'followers' => $this->user->follow,
