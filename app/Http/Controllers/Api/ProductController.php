ece811d5a2ba32784468c62bb4616e908ebe2076
umermuk <umerwell1234@gmail.com>
2022-11-06 23:41:45 +0500

    working

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -190,9 +190,9 @@ class ProductController extends Controller
 
     public function appWholesalerProducts()
     {
-        $wholesalers = User::has('products')->with(['role', 'products' => function ($query) {
-            $query->latest()->take(10);
-        }])->where('role_id', 4)->get();
+        $wholesalers = User::with(['role', 'products' => function ($query) {
+            $query->latest()->take(6);
+        }])->where('role_id', 4)->has('products')->get();
         if (count($wholesalers)) return response()->json(['status' => true, 'wholesalers' => $wholesalers ?? []], 200);
         return response()->json(['status' => false, 'Message' => 'not found']);
     }
