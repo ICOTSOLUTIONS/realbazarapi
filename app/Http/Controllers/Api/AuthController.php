161c1e1c0fe0b63569a00184f8b169a8353fd440
umermuk <umerwell1234@gmail.com>
2022-11-18 01:36:33 +0500

    working

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -221,8 +221,8 @@ class AuthController extends Controller
                     $user->bill_image = "bill/" . $filename;
                 }
                 if (!empty($request->referral_code)) {
-                    dd($request->referral_code);
                     $referr_user = User::where('referral_code', $request->referral_code)->first();
+                    dd($referr_user);
                     if (empty($referr_user)) throw new Error('Referral Code not valid');
                     $referr_user->referral_count += 1;
                     if (!$referr_user->save()) throw new Error('User not Register to this Referral Code');
