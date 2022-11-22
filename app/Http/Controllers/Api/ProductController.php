8375f62e61ef5c7568778f91007924e611349581
umermuk <umerwell1234@gmail.com>
2022-11-23 00:50:04 +0500

    working

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -969,7 +969,7 @@ class ProductController extends Controller
         $seller_top_sales = User::whereHas('role', function ($query) {
             $query->where('name', 'retailer')->orWhere('name', 'wholesaler');
         })->with('sellers_orders')->get()->sortBy(function ($hackathon) {
-            return $hackathon->sellers_orders->withCount('user_orders');
+            return $hackathon->sellers_orders()->user_orders()->count();
         });
         // dd($seller_top_sales);
         // $seller_top_sales = Order::selectRaw('seller_id, SUM(net_amount) as total_amount')
