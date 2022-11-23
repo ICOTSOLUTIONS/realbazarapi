90a18fc12eda35819a40f2b1c667d47bba3e0d36
umermuk <umerwell1234@gmail.com>
2022-11-23 21:14:42 +0500

    working

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -968,8 +968,8 @@ class ProductController extends Controller
     {
         $seller_top_sales = User::withCount('sellers_orders_products')->whereHas('role', function ($query) {
             $query->where('name', 'retailer')->orWhere('name', 'wholesaler');
-        })->take(10)->get();
-        $seller_top_sales = $seller_top_sales->sortByDesc('sellers_orders_products_count')->values();
+        })->get();
+        $seller_top_sales = $seller_top_sales->sortByDesc('sellers_orders_products_count')->take(10)->values();
         if (count($seller_top_sales)) return response()->json(["status" => true, 'seller_top_sales' => $seller_top_sales ?? []], 200);
         else return response()->json(["status" => false, 'seller_top_sales' => $seller_top_sales ?? []]);
     }
