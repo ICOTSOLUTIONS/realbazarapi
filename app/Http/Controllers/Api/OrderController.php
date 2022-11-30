6aeb0ec80cf60d2aca8be7eab9c0e2f103a3749e
umermuk <umerwell1234@gmail.com>
2022-11-30 23:47:25 +0500

    working

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -294,7 +294,9 @@ class OrderController extends Controller
         $pp_SecureHash = $this->get_SecureHash($post_data);
 
         $post_data['pp_SecureHash'] = $pp_SecureHash;
-        return view('jazzcash_checkout_form', ['post_data' => $post_data]);
+        return redirect()->away('https://sandbox.jazzcash.com.pk/CustomerPortal/transactionmanagement/merchantform/')->with($post_data);
+
+        // return view('jazzcash_checkout_form', ['post_data' => $post_data]);
     }
 
     private function get_SecureHash($data_array)
