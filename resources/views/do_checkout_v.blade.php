71864e02620f191865c242db16e2c7a12f5cd8f0
umermuk <umerwell1234@gmail.com>
2022-12-07 23:26:58 +0500

    working

--- resources/views/do_checkout_v.blade.php
+++ resources/views/do_checkout_v.blade.php
@@ -5,7 +5,7 @@
 </script>
 </head>
 <body onload="closethisasap();">
-    <form name="redirectpost" method="POST" action="{{ Config::get('jazzcashCheckout.jazzcash.CARD_REFUND_POST_URL') }}">
+    <form name="redirectpost" method="POST" action="{{ Config::get('jazzcashCheckout.jazzcash.TRANSACTION_POST_URL') }}">
         @foreach ($post_data as $key => $value)
             <input type="hidden" name="{{ $key }}" value="{{ $value }}">
         @endforeach
