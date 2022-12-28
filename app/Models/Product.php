c9d6318092d119cc4a6b1c649c0b1480f02944d6
umermuk <umerwell1234@gmail.com>
2022-12-28 22:01:58 +0500

    working

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = ['category', 'status'];
-    protected $appends = ["threeStar"];
+    protected $appends = ["threeStar","tag"];
 
     public function user()
     {
@@ -82,5 +82,9 @@ class Product extends Model
         $three_star = 0;
         $three_star = ProductReview::where('product_id', $this->id)->where('stars','>=','3')->count();
         return $three_star;
+    }
+    protected function getTagAttribute()
+    {
+        return json_decode($this->tags);
     }
 }
