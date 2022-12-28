1a6004e5ec98d7151f87ad088b6c46766332f5a7
umermuk <umerwell1234@gmail.com>
2022-12-28 22:12:43 +0500

    working

--- app/Http/Resources/ProductsResource.php
+++ app/Http/Resources/ProductsResource.php
@@ -19,7 +19,7 @@ class ProductsResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'tags' => str_replace('#','',$this->tag),
+            'tags' => preg_match("/[^A-Za-z0-9\,]/", "", $this->tag),
             'price' => $this->price,
             'discount' => $this->discount_price,
             'product_description' => $this->desc,
