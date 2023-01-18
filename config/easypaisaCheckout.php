8ee1f26fa884b9b14127180a438da5ac5a86210b
umermuk <umerwell1234@gmail.com>
2023-01-18 22:12:30 +0500

    working

--- config/easypaisaCheckout.php
+++ config/easypaisaCheckout.php
@@ -2,9 +2,9 @@
 
 return [
     'easypaisa' => [
-        'STORE_ID'      => '21121',
+        'STORE_ID'      => '393592',
         'TransactionType'          => 'InitialRequest',
-        'HASH_KEY' => 'O81PIDOAT6E8XCOH',
+        'HASH_KEY' => 'Y5BKKD5VLXZHT174',
 
         'POST_BACK_URL'  => 'https://real-bazar-web.vercel.app/account/payment',
         'TRANSACTION_POST_URL'  => 'https://easypaystg.easypaisa.com.pk/tpg/?',
