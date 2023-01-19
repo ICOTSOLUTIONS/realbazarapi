fbcd68537d58d13df8584849641c2593fbe4e679
umermuk <umerwell1234@gmail.com>
2023-01-19 21:06:27 +0500

    Working

--- config/easypaisaCheckout.php
+++ config/easypaisaCheckout.php
@@ -7,7 +7,7 @@ return [
         'HASH_KEY' => 'Y5BKKD5VLXZHT174',
 
         'POST_BACK_URL'  => 'https://realbazar.pk/account/payment',
-        'TRANSACTION_POST_URL'  => 'https://easypaystg.easypaisa.com.pk/tpg/?',
+        'TRANSACTION_POST_URL'  => 'https://easypay.easypaisa.com.pk/tpg/?',
 
     ]
 ];
