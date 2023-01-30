43d7f1e7519b6f99129241c741348bbdcc2fd04f
ICOTSOLUTIONS <102804878+ICOTSOLUTIONS@users.noreply.github.com>
2023-01-30 20:30:14 +0500

    postback url changed

--- config/easypaisaCheckout.php
+++ config/easypaisaCheckout.php
@@ -6,7 +6,8 @@ return [
         'TransactionType'          => 'InitialRequest',
         'HASH_KEY' => 'Y5BKKD5VLXZHT174',
 
-        'POST_BACK_URL'  => 'https://realbazar.pk/account/payment',
+        // 'POST_BACK_URL'  => 'https://realbazar.pk/account/payment',
+        'POST_BACK_URL'  => 'https://realbazar.pk',
         'TRANSACTION_POST_URL'  => 'https://easypay.easypaisa.com.pk/tpg/?',
 
     ]
