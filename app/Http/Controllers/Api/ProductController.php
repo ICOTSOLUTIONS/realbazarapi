4995aa21bc95aa9020e5526eef7c082e2751d2db
umermuk <umerwell1234@gmail.com>
2023-08-17 02:16:55 +0500

    working

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -458,65 +458,62 @@ class ProductController extends Controller
 
     public function searchLocation(Request $request)
     {
-        if (!isset($request->lat) || !isset($request->lng) || !isset($request->distance)) return response()->json(['status' => false, 'Message' => 'Location Required', 'Products' => []]);
-        $latitude  = $request->lat;
-        $langitude  = $request->lng;
-        $users = User::all();
-        $distance = $request->distance;  //(miles - see note)
-        $arr_user = [];
-        foreach ($users as $user) {
-            if(!empty($user->lat) || !empty($user->lng)){
-                $delta_lat = $user->lat - $latitude;
-                $delta_lon = $user->lng - $langitude;
+        $product = [];
+        $query = Product::has('user')->with(['user', 'images', 'variation', 'subCategories.categories', 'reviews.users']);
+        if(isset($request->name) && !empty($request->name)){
+            $names = explode(',', $request->name);
+            foreach ($names as $tag) {
+                $query->where('title', 'LIKE', '%' . $tag . '%')->orWhere('tags', 'LIKE', '%' . $tag . '%');
+            }
+        }
 
-                $earth_radius = 6372.795477598;
+        if(isset($request->sub_category)) $query->where('sub_category_id',$request->sub_category);
+        if(isset($request->price_from) && isset($request->price_to)){
+            $query->whereHas('variation', function ($q) use($request) {
+                    $q->whereBetween('price', [$request->price_from, $request->price_to]);
+            });
+        }
 
-                $alpha    = $delta_lat / 2;
-                $beta     = $delta_lon / 2;
-                $a        = sin(deg2rad($alpha)) * sin(deg2rad($alpha)) + cos(deg2rad($latitude)) * cos(deg2rad($user->lat_from)) * sin(deg2rad($beta)) * sin(deg2rad($beta));
-                $c        = asin(min(1, sqrt($a)));
-                $distance1 = 2 * $earth_radius * $c;
-                $distance1 = $distance1 * 0.621371;
-                if ($distance > $distance1) {
-                    $arr_user[] = $user->id;
+        if (isset($request->lat) && isset($request->lng) && isset($request->distance)){
+            $latitude  = $request->lat;
+            $langitude  = $request->lng;
+            $users = User::all();
+            $distance = $request->distance;  //(miles - see note)
+            $arr_user = [];
+            foreach ($users as $user) {
+                if(!empty($user->lat) || !empty($user->lng)){
+                    $delta_lat = $user->lat - $latitude;
+                    $delta_lon = $user->lng - $langitude;
+
+                    $earth_radius = 6372.795477598;
+
+                    $alpha    = $delta_lat / 2;
+                    $beta     = $delta_lon / 2;
+                    $a        = sin(deg2rad($alpha)) * sin(deg2rad($alpha)) + cos(deg2rad($latitude)) * cos(deg2rad($user->lat_from)) * sin(deg2rad($beta)) * sin(deg2rad($beta));
+                    $c        = asin(min(1, sqrt($a)));
+                    $distance1 = 2 * $earth_radius * $c;
+                    $distance1 = $distance1 * 0.621371;
+                    if ($distance > $distance1) {
+                        $arr_user[] = $user->id;
+                    }
                 }
             }
         }
-        if (!count($arr_user) > 0) return response()->json(['status' => false, 'Message' => 'Products not found', 'Products' => []]);
 
         if ($request->role == 'retailer') {
-            $query = Product::has('user')->with(['user', 'images', 'variation', 'subCategories.categories', 'reviews.users']);
-            if(isset($request->sub_category)) $query->where('sub_category_id',$request->sub_category);
-            if(isset($request->price_from) && isset($request->price_to)){
-                $query->whereHas('variation', function ($q) use($request) {
-                        $q->whereBetween('price', [$request->price_from, $request->price_to]);
-                });
-            }
-            $product = $query->whereIn('user_id',$arr_user)
-            ->whereHas('user', function ($q1) {
+            if (!empty($arr_user) && count($arr_user) > 0) $query->whereIn('user_id',$arr_user);
+            $product = $query->whereHas('user', function ($q1) {
                 $q1->whereRelation('role', 'name', 'retailer');
             })->get();
         } else if ($request->role == 'wholesaler') {
-            $query = Product::has('user')->with(['user', 'images', 'variation', 'subCategories.categories', 'reviews.users']);
-            if(isset($request->sub_category)) $query->where('sub_category_id',$request->sub_category);
-            if(isset($request->price_from) && isset($request->price_to)){
-                $query->whereHas('variation', function ($q) use($request) {
-                        $q->whereBetween('price', [$request->price_from, $request->price_to]);
-                });
-            }
-            $product = $query->whereIn('user_id',$arr_user)
-            ->whereHas('user', function ($q1) {
+            if (!empty($arr_user) && count($arr_user) > 0) $query->whereIn('user_id',$arr_user);
+            $product = $query->whereHas('user', function ($q1) {
                 $q1->whereRelation('role', 'name', 'wholesaler');
             })->get();
+
         } else {
-            $query = Product::has('user')->with(['user', 'images', 'variation', 'subCategories.categories', 'reviews.users']);
-            if(isset($request->sub_category)) $query->where('sub_category_id',$request->sub_category);
-            if(isset($request->price_from) && isset($request->price_to)){
-                $query->whereHas('variation', function ($q) use($request) {
-                        $q->whereBetween('price', [$request->price_from, $request->price_to]);
-                });
-            }
-            $product = $query->whereIn('user_id',$arr_user)->get();
+            if (!empty($arr_user) && count($arr_user) > 0) $query->whereIn('user_id',$arr_user);
+            $product = $query->get();
         }
         if (count($product)) return response()->json(['status' => true, 'Message' => 'Product found', 'Products' => ProductsResource::collection($product)], 200);
         else return response()->json(['status' => false, 'Message' => 'Product not found', 'Products' => $product ?? []]);
