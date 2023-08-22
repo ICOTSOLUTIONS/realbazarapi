fc22d235bab482f9e651f5fee8c2f4ab23253821
umermuk <umerwell1234@gmail.com>
2023-08-23 03:52:20 +0500

    working

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -462,7 +462,7 @@ class ProductController extends Controller
         $query = Product::has('user')->with(['user', 'images', 'variation', 'subCategories.categories', 'reviews.users']);
         if(isset($request->category) && !empty($request->category)) $query->whereRelation('subCategories.categories', 'id', $request->category);
         if(isset($request->sub_category) && !empty($request->sub_category)) $query->where('sub_category_id',$request->sub_category);
-        if(isset($request->price_from) && isset($request->price_to) && !empty($request->price_from) && !empty($request->price_to)){
+        if(isset($request->price_from) && isset($request->price_to)){
             $query->whereHas('variation', function ($q) use($request) {
                     $q->whereBetween('price', [$request->price_from, $request->price_to]);
             });
@@ -496,7 +496,7 @@ class ProductController extends Controller
         if(isset($request->name) && !empty($request->name)){
             $names = explode(',', $request->name);
             foreach ($names as $tag) {
-                $query->where('title', 'LIKE', '%' . $tag . '%');
+                $query->where('title', 'LIKE', '%' . $tag . '%')->orWhere('tags', 'LIKE', '%' . $tag . '%');
             }
         }
         if ($request->role == 'retailer') {
